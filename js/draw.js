53e5d55014eb6f4b4643e70c51389393adf9474c
Katie Wilday <wildayk@gmail.com>
2020-04-21 17:52:04 -0400

    adding reset button

--- js/draw.js
+++ js/draw.js
@@ -99,6 +99,7 @@ var drawModule = (function () {
       for(var i = 0; i < array.length; i++) {
         if(array[i].x === x && array[i].y === y)
         return true;
+        
       }
       return false;
   }
